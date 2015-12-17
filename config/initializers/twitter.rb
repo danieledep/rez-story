3d7b67650077ea60e3a96b9c827ad18ff3ef4b46
Daniel Cardenas <dan85.cardenas@gmail.com>
2015-12-16 23:17:53 -0500

    added simple html twitter timeline

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CXVNsTDohsJaIxl0cjpuLKXYr']
+  config.consumer_secret = ENV['Y49dNi2NPN9vJaPS95QnRLslOqisEuC7v934lHOfN05cVjbtDB']
+  config.access_token = ENV['2834545563-QYQqm8hnLPiU3eFyAD8SGtKhfIYW7gMp8fGh8Xd']
+  config.access_token_secret = ENV['SUquQt3XC2ve3IIa8JbwMa4bsRCpZSJuCVKYAXLUTDBBT']
+end
