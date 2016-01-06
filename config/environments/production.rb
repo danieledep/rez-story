532886ef4d0bec497a40cbfa3025fe26837ef6b3
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-01-05 21:38:47 -0500

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Twitter API config
+  config.cache_classes = false
+  config.eager_load = false
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+  config.twitter_key = "CXVNsTDohsJaIxl0cjpuLKXYr"
+  config.twitter_secret = "Y49dNi2NPN9vJaPS95QnRLslOqisEuC7v934lHOfN05cVjbtDB"
 end
